853797954205c50faf79145c6b3aa6848efcefca
Martin Bies <MBies87@googlemail.com>
2022-08-09 11:46:25 +0200

    Fix tests and doctests

--- test/doctest.jl
+++ test/doctest.jl
@@ -1,5 +1,9 @@
-using Documenter, Oscar, CYTools
+using Pkg
+Pkg.add("Oscar")
+using Oscar
+using Documenter
+using CYTools
 
-DocMeta.setdocmeta!(CYTools, :DocTestSetup, :(using CYTools, Random); recursive = true)
+DocMeta.setdocmeta!(CYTools, :DocTestSetup, :(using CYTools, Random; using Oscar); recursive = true)
 
 doctest(CYTools)
