6a2fb1e299fb48322174e33b6fa317f5df7d27fd
Martin Bies <MBies87@googlemail.com>
2022-08-09 14:09:13 +0200

    Further improvements on the documentation
    Higher test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using Test
 using CYTools
 using Oscar
 
-cy = CYHypersurface([1 0; 0 1; -1 -1])
 
 # Add better tests here.
 @testset "Generic test" begin
+    cy = CYHypersurface([1 0; 0 1; -1 -1])
     @test ambient_dim(polytope(cy)) == 2
 end
